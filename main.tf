584b311e62159fd7e37133360966084be13b42ef
ofmw <ofmw91@gmail.com>
2024-03-07 12:27:59 +0900

    Create IGW,NGW

--- main.tf
+++ main.tf
@@ -73,3 +73,20 @@ data "aws_subnet" "aws-pvt-sub-a" {
     values = ["aws-pvt-sub-a"]
   }
 }
+
+#Create Internet Gatway
+resource "aws_internet_gateway" "def-igw" {
+  vpc_id = aws_vpc.def-vpc.id
+  tags = {
+    Name = "tf-def-igw"
+  }
+}
+
+#Create NAT Gatway
+resource "aws_nat_gateway" "pvt-ngw-a" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.pub-sub-a.id
+  tags = {
+    Name = "tf-pvt-ngw-a"
+  }
+}
