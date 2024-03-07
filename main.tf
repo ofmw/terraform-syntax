879fa9de127894ee5e938eb437c0a6a452de3bfe
ofmw <ofmw91@gmail.com>
2024-03-07 10:56:53 +0900

    Create Private Subnet & Change cidr_block

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "def-vpc" {
 #Create Public Subnet
 resource "aws_subnet" "pub-sub-a" {
   vpc_id            = aws_vpc.def-vpc.id
-  cidr_block        = "10.10.1.0/24"
+  cidr_block        = "10.10.11.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "tf-pub-sub-a"
@@ -32,9 +32,28 @@ resource "aws_subnet" "pub-sub-a" {
 
 resource "aws_subnet" "pub-sub-c" {
   vpc_id            = aws_vpc.def-vpc.id
-  cidr_block        = "10.10.2.0/24"
+  cidr_block        = "10.10.12.0/24"
   availability_zone = "us-east-1c"
   tags = {
     Name = "tf-pub-sub-c"
+  }
+}
+
+#Create Private Subnet
+resource "aws_subnet" "pvt-sub-a" {
+  vpc_id            = aws_vpc.def-vpc.id
+  cidr_block        = "10.10.13.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "tf-pvt-sub-a"
+  }
+}
+
+resource "aws_subnet" "pvt-sub-c" {
+  vpc_id            = aws_vpc.def-vpc.id
+  cidr_block        = "10.10.14.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "tf-pvt-sub-c"
   }
 }
