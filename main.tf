4e294c7561cc932ea710acc7ef232ff3dac879e4
ofmw <ofmw91@gmail.com>
2024-03-07 10:49:41 +0900

    Create Public Subnet

--- main.tf
+++ main.tf
@@ -19,3 +19,22 @@ resource "aws_vpc" "def-vpc" {
     Name = "tf-def-vpc"
   }
 }
+
+#Create Public Subnet
+resource "aws_subnet" "pub-sub-a" {
+  vpc_id            = aws_vpc.def-vpc.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "tf-pub-sub-a"
+  }
+}
+
+resource "aws_subnet" "pub-sub-c" {
+  vpc_id            = aws_vpc.def-vpc.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "tf-pub-sub-c"
+  }
+}
